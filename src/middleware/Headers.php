86921450c692d70b005b64209d79ba218cef32b0
Fal-cod <abdullahfalus@gmail.com>
2018-08-18 10:41:07 +0300

    test Headers

--- src/middleware/Headers.php
+++ src/middleware/Headers.php
@@ -5,21 +5,3 @@
 
 
 
-$withHeader =  function ($request, $response, $next) {
-
-    $response = $next($request, $response);
-    return $response
-        ->withAddedHeader('Content-Type','application/json; charset=utf-8')
-        ->withHeader('Access-Control-Allow-Origin', '*')
-        ->withHeader('Access-Control-Allow-Credentials', 'true')
-        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization,DNT,X-CustomHeader,Keep-Alive,User-Agent,If-Modified-Since,Cache-Control')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-};
-
-
-
-
-$app->add($withHeader);
-
-
-
