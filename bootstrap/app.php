acfa9bed5199811f86d41bc4ca9f4b425cd0b088
Fal-cod <abdullahfalus@gmail.com>
2018-08-18 10:49:16 +0300

    tset Header Access-Control-Allow-Origin 7

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -52,20 +52,6 @@ $container['db'] = new dbConnect();
 $container['upload_directory'] =  $_SERVER['DOCUMENT_ROOT'] . '/uploads';
 
 
-$app->options('/{routes:.+}', function ($request, $response, $args) {
-    return $response;
-});
-
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    return $response
-    ->withAddedHeader('Content-Type','application/json; charset=utf-8')
-    ->withHeader('Access-Control-Allow-Origin', '*')
-    ->withHeader('Access-Control-Allow-Credentials', 'true')
-    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization,DNT,X-CustomHeader,Keep-Alive,User-Agent,If-Modified-Since,Cache-Control')
-    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-});
-
 
 
 
@@ -115,6 +101,24 @@ require  ('../src/routes/REvaluationResults.php');
 
 require  ('../src/middleware/auth.php');
 
-require  ('../src/middleware/Headers.php');
+//require  ('../src/middleware/Headers.php');
 
 require  ('../src/routes/RLogin.php');
+
+
+
+
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+    ->withAddedHeader('Content-Type','application/json; charset=utf-8')
+    ->withHeader('Access-Control-Allow-Origin', '*')
+    ->withHeader('Access-Control-Allow-Credentials', 'true')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization,DNT,X-CustomHeader,Keep-Alive,User-Agent,If-Modified-Since,Cache-Control')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
