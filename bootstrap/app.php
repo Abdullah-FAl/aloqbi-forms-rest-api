fca74251bcec654cb3eb995c4261497e09f72e20
Fal-cod <abdullahfalus@gmail.com>
2018-08-18 10:46:55 +0300

    tset Header 6

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -52,25 +52,21 @@ $container['db'] = new dbConnect();
 $container['upload_directory'] =  $_SERVER['DOCUMENT_ROOT'] . '/uploads';
 
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
 
-
-
-
-$withHeader =  function ($request, $response, $next) {
-
-    $response = $next($request, $response);
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
     return $response
-        ->withAddedHeader('Content-Type','application/json; charset=utf-8')
-        ->withHeader('Access-Control-Allow-Origin', '*')
-        ->withHeader('Access-Control-Allow-Credentials', 'true')
-        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization,DNT,X-CustomHeader,Keep-Alive,User-Agent,If-Modified-Since,Cache-Control')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-};
+    ->withAddedHeader('Content-Type','application/json; charset=utf-8')
+    ->withHeader('Access-Control-Allow-Origin', '*')
+    ->withHeader('Access-Control-Allow-Credentials', 'true')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization,DNT,X-CustomHeader,Keep-Alive,User-Agent,If-Modified-Since,Cache-Control')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
 
 
-
-
-$app->add($withHeader);
 
 
 
