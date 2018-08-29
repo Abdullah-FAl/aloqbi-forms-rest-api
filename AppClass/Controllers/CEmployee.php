4110c73bc8fa7bf9d229289cb0c3e7998fc12d5b
Fal-cod <abdullahfalus@gmail.com>
2018-08-29 15:03:38 +0300

    <===edit function UpdateEmployeeInfo  to add img and tasks to EmployeeInfo===>

--- AppClass/Controllers/CEmployee.php
+++ AppClass/Controllers/CEmployee.php
@@ -2,7 +2,7 @@
 
 
 namespace AppClass\Controllers;
-use Firebase\JWT\JWT;
+
 
 class CEmployee extends Base
 {
@@ -157,6 +157,7 @@ $sql2 = "SELECT TEpm.employee_id,
 
          $uploadedFiles = $request->getUploadedFiles();
          $parsedBody = $request->getParsedBody();
+         
 
         /*=================={Bring the user data from a database}================*/
 
@@ -248,39 +249,93 @@ $sql2 = "SELECT TEpm.employee_id,
 
 
 
+                        $uploadedFiles = $request->getUploadedFiles();
+                        $parsedBody = $request->getParsedBody();
+                        $epmID      =  (int)$parsedBody['employee_id'];
 
-        $userData = $request->getParsedBody();
+                    
 
-
-
-        /*=================={Bring the user data from a database}================*/
-
-
-        $sql = "UPDATE  employee SET employee_name = :emp_name WHERE employee_id = :emp_id";
+                    /*=================={Bring the user data from a database}================*/
 
 
-        // Get DB Object
-        $db = $this->db;
-        // Connect
-        $db = $db->connect();
-        //sql prepare
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam(':emp_id',$userData['employee_id']);
-        $stmt->bindParam(':emp_name',$userData['employee_name']);
-        $sql_result = $stmt->execute();
-        // exit Connect
-        $db = null;
+                    $sql = "UPDATE  employee 
+                    SET employee_name      = :emp_name, 
+                    employee_number        = :emp_number,
+                    employee_job_name      = :emp_job_name,
+                    employee_email         = :emp_email,
+                    employee_phone_number  = :emp_phone_number
+                    WHERE employee_id      = :emp_id";
+
+
+                    // Get DB Object
+                    $db = $this->db;
+                    // Connect
+                    $db = $db->connect();
+                    //sql prepare
+                    $stmt = $db->prepare($sql);
+                    $stmt->bindParam(':emp_id',$epmID);
+                    $stmt->bindParam(':emp_name',$parsedBody['employee_name']);
+                    $stmt->bindParam(':emp_number',$parsedBody['employee_number']);
+                    $stmt->bindParam(':emp_job_name',$parsedBody['employee_job_name']);
+                    $stmt->bindParam(':emp_email',$parsedBody['employee_email']);
+                    $stmt->bindParam(':emp_phone_number',$parsedBody['employee_phone_number']);
+                    $sql_result1 = $stmt->execute();
+                    // exit Connect
+                    $db = null;
+
+
+                    if($sql_result1) {
+
+
+                        $afteraddfiles = $this->UpdeatEmployeeFiles($uploadedFiles,$epmID);
+
+                        if (!empty($parsedBody['employee_tasks_ids']) ){
+
+
+                            $sql = "DELETE FROM employee_tasks WHERE employee_id = ?";
+                                
+                                    // Get DB Object
+                                    $db = $this->db;
+                                    // Connect
+                                    $db = $db->connect();
+                                    //sql prepare
+                                    $stmt = $db->prepare($sql);
+                                    $sql_result2 = $stmt->execute([$epmID]);
+
+                                            if ($sql_result2) {
+                                                foreach ($parsedBody['employee_tasks_ids'] as $Taskid) {
+                                                    $sql = "INSERT INTO employee_tasks 
+                                                                            (employee_id,tasks_id)
+                                                                    VALUES (:emp_id,:tsk_id)";
+
+                                                    //sql prepare
+                                                    $stmt = $db->prepare($sql);
+                                                    $stmt->bindParam(':emp_id', $epmID);
+                                                    $stmt->bindParam(':tsk_id', $Taskid);
+                                                    $sql_result2 = $stmt->execute();
+
+                                                }
+                                                    // exit Connect
+                                                    $db = null;
+                                            }
+                        
+                        
+        
+             }
+
+       }
 
 
 
-        $responsedata = array([
 
-            'cod'  => 200,
-            'data' =>$sql_result
+        // $responsedata = array([
 
-        ]);
+        //     'cod'  => 200,
+        //     'data' => $sql_result2
 
-        return $response->withJson($responsedata,200);
+        // ]);
+
+        //  return $response->withJson($responsedata,200);
 
 
     }
@@ -419,7 +474,132 @@ $sql2 = "SELECT TEpm.employee_id,
 
 
 
+ //// updeat epm img
+    ///
 
+    private  function   UpdeatEmployeeFiles($uploadedFilesarry,$epmID){
+
+
+
+        if (!empty($uploadedFilesarry['employeeimgfile'])) {
+
+                // Get DB Object
+                $db = $this->db;
+                // Connect
+                $db = $db->connect();
+
+            $sql1 = "SELECT file_path FROM employee_files WHERE employee_id = ?";
+                //sql prepare
+                $stmt = $db->prepare($sql1);
+                $stmt->execute([$epmID]);
+               $sql_result4 = $stmt->fetch();
+           
+                if ($sql_result4) {
+                    $parts     = explode('/', $sql_result4->file_path);
+                    $file_name  = end($parts);
+                    $directory = $this->upload_directory;
+                    $file_for_delete =  realpath($directory). '/employee/img/'. $file_name;
+     
+                 
+
+                 
+                    if (file_exists($file_for_delete)) {
+                        if (unlink($file_for_delete)) {
+
+
+
+            // handle single input with multiple file uploads
+                            foreach ($uploadedFilesarry['employeeimgfile'] as $uploadedfile) {
+                                if ($uploadedfile->getError() === UPLOAD_ERR_OK) {
+                                    $md5Hash = md5(time());
+                                    $directory = $this->upload_directory;
+                                    $file_name = $uploadedfile->getClientFilename();
+                                    $parts     = explode('.', $file_name);
+                                    $file_ext  = end($parts);
+                                    $uploadedfile->moveTo($directory . '/employee/img/' . $md5Hash . $epmID .'.'.$file_ext);
+                                    $file_type = $uploadedfile->getClientMediaType();
+                                    $SERVER_path = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]/uploads/employee/img/";
+                                    $file_path = $SERVER_path . $md5Hash . $epmID .'.'.$file_ext;
+
+                                    $sql2 = "UPDATE  employee_files 
+                    SET 
+                    file_name              = :f_name, 
+                    file_type              = :f_type,
+                    file_path              = :f_path
+                    WHERE employee_id      = :emp_id";
+
+
+
+
+                                    //sql prepare
+                                    $stmt = $db->prepare($sql2);
+                                    $stmt->bindParam(':emp_id', $epmID);
+                                    $stmt->bindParam(':f_name', $file_name);
+                                    $stmt->bindParam(':f_type', $file_type);
+                                    $stmt->bindParam(':f_path', $file_path);
+                                    $sql_result = $stmt->execute();
+                                }
+                            }
+                        }
+                    } else {
+                      
+                    }
+                }else{
+                  
+                        // handle single input with multiple file uploads
+                        foreach ($uploadedFilesarry['employeeimgfile'] as $uploadedfile) {
+            
+                            if ($uploadedfile->getError() === UPLOAD_ERR_OK) {
+            
+                                $md5Hash = md5(time());
+                                $directory = $this->upload_directory;
+                                $file_name = $uploadedfile->getClientFilename();
+                                $parts     = explode('.', $file_name);
+                                $file_ext  = end($parts);
+                                $uploadedfile->moveTo($directory . '/employee/img/' . $md5Hash . $epmID .'.'.$file_ext);
+                                $file_type = $uploadedfile->getClientMediaType();
+                                $SERVER_path = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]/uploads/employee/img/";
+                                $file_path = $SERVER_path . $md5Hash . $epmID .'.'.$file_ext;
+            
+            
+            
+                                $sql4 = "INSERT INTO employee_files (employee_id,file_name,file_type,file_path)
+                                          VALUES (:emp_id,:f_name,:f_type,:f_path)";
+            
+                                //sql prepare
+                                $stmt = $db->prepare($sql4);
+                                $stmt->bindParam(':emp_id', $epmID);
+                                $stmt->bindParam(':f_name', $file_name);
+                                $stmt->bindParam(':f_type', $file_type);
+                                $stmt->bindParam(':f_path', $file_path);
+                                $sql_result = $stmt->execute();
+            
+            
+            
+                            }}
+
+
+
+
+                }
+           
+
+
+
+
+
+        }else {
+          
+            $sql_result = false;
+        }
+
+
+       
+                $db = null;
+          
+
+        return $sql_result;
+    }
 
 
 
--- src/routes/REmployee.php
+++ src/routes/REmployee.php
@@ -9,7 +9,7 @@ $app->group('/api/v1/employee',function() use ($app){
     $this->get('/info',                     (CEmployee::class . ':GetAllEmployeeInfo'));
     $this->get('/{employee_id}/tasks',      (CEmployee::class . ':GetEmployeeTasks'));
     $this->post('/add',                     (CEmployee::class . ':AddNewEmployeeInfo'));
-    $this->put('/update',                   (CEmployee::class . ':UpdateEmployeeInfo'));
+    $this->post('/update',                   (CEmployee::class . ':UpdateEmployeeInfo'));
     $this->delete('/delete',                (CEmployee::class . ':DeleteEmployeeInfo'));
 });
 
