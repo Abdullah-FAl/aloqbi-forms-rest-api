46e338afa352aa52d31d3da49e55fbce839dfc53
Fal-cod <abdullahfalus@gmail.com>
2018-09-17 14:04:42 +0300

    test => Access-Control-Allow-Origin ;

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
-
-require  ('../bootstrap/app.php');
 header_remove("X-Powered-By");
+require  ('../bootstrap/app.php');
+
 
 $app->run();
 
--- src/middleware/Headers.php
+++ src/middleware/Headers.php
@@ -10,11 +10,10 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 });
 
 $app->add(function ($req, $res, $next) {
-    $SERVER_path = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
     $response = $next($req, $res);
     return $response
     ->withAddedHeader('Content-Type','application/json; charset=utf-8')
-    ->withHeader('Access-Control-Allow-Origin',$SERVER_path)
+    ->withHeader('Access-Control-Allow-Origin', '*')
     ->withHeader('Access-Control-Allow-Credentials', 'true')
     ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization,DNT,X-CustomHeader,Keep-Alive,User-Agent,If-Modified-Since,Cache-Control')
     ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
